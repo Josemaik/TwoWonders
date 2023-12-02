6c119f12259cff156c66cb3dd5a99558ad56271e
Julio Sala Gallardo <juliosalag@gmail.com>
2023-12-02 14:13:01 +0100

    Creacion life_component.cpp

---
+++ src/components/life_component.cpp
@@ -0,0 +1,8 @@
+#include "life_component.hpp"
+
+void LifeComponent::decreaseLife(){
+    if((elapsed >= countdown) && (life > 0)){
+        life -= 1;
+        elapsed = 0;
+    }
+}
--- src/components/life_component.hpp
+++ src/components/life_component.hpp
@@ -5,14 +5,6 @@ struct LifeComponent
     int life{};
     float countdown{ 1.0f }, elapsed{ 1.0f }; // En segundos
 
-    void decreaseLife(){
-        if((elapsed >= countdown) && (life > 0)){
-            life -= 1;
-            elapsed = 0;
-        }
-    }
-
-    void decreaseCountdown(){
-        elapsed += GetFrameTime(); // delta time
-    }
+    void decreaseLife();
+    void decreaseCountdown(){ elapsed += GetFrameTime(); }; // delta time
 };
