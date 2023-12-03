9e6f61be2995febef65fa1a274570331d3be51ae
Julio Sala Gallardo <juliosalag@gmail.com>
2023-12-04 00:02:42 +0100

    Bug fix

--- src/components/collider_component.hpp
+++ src/components/collider_component.hpp
@@ -7,7 +7,7 @@ enum BehaviorType {
     PLAYER = 0x01,
     ENEMY = 0x02,
     STATIC = 0x04,
-    ATTACK = 0x08,
+    ATTACK_PLAYER = 0x08,
 };
 
 struct ColliderComponent
--- src/systems/life_system.cpp
+++ src/systems/life_system.cpp
@@ -8,7 +8,9 @@ void LifeSystem::update(EntityManager& em) {
 
     // Destruir las entidades que tengan vida a 0
     size_t count = 0;
-    for (auto const& e : em.getEntities())
+    for (auto const& e : em.getEntities()){
         if (e.hasComponent<LifeComponent>() &&  em.getComponent<LifeComponent>(e).life == 0)
             em.destroyEntity(count);
+        count += 1;
+    }
 }
