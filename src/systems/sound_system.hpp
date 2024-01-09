02cf34d92283f7315abd4d8e79a256027c9d25c3
solsolet <gsl21@alu.ua.es>
2024-01-09 21:26:10 +0100

    cabeceras FMOD

---
+++ src/systems/sound_system.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "../utils/fmod/fmod_studio.h" // C
+#include "../utils/fmod/fmod_studio.hpp" // C ++
+#include "../utils/fmod/fmod.hpp" // low - level api
+#include "../utils/fmod/fmod_errors.h"
