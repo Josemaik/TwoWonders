9bd5b915fb90a97e079146717098770456de5ebb
JoseManuelSoriano <jmsb18@gcloud.ua.es>
2024-01-23 12:43:37 +0100

    estructura para guardar datos de los slimes en blackoard

--- src/utils/sngtn/blackboard.hpp
+++ src/utils/sngtn/blackboard.hpp
@@ -2,8 +2,13 @@
 
 #include <cstdint>
 #include <utils/path.hpp>
-#include "ai_component.hpp"
+#include <utils/vec3D.hpp>
 
+//Estructura para almacenar información de una IA Slime
+struct SlimeInfo {
+    vec3d position{};
+    int life{};
+};
 struct BlackBoard_t {
     double tx { 0.0 } , tz { 0.0 };
     // SB behaviour { SB::Arrive };
@@ -12,4 +17,13 @@ struct BlackBoard_t {
     std::size_t teid{};
     //Path
     Path_t<4> path { vec3d{8.0, 0.0, 4.0} , {3.0,0.0,4.0} , {3.0,0.0,5.0}, {8.0,0.0,5.0} };
+    //Actualizar información IA slimes
+    void updateSlimeInfo (std::size_t slime_id,vec3d position, int life){
+        SlimeInfo slimeInfo(position,life);
+        slimeData[slime_id] = slimeInfo;
+        // std::cout << "life:" <<slimeData[slime_id].life << "posx:" <<slimeData[slime_id].position.x() << "\n";
+    }
+    private:
+        std::unordered_map<std::size_t, SlimeInfo> slimeData;
+
 };
