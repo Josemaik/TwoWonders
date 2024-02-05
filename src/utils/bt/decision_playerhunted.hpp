2350df537ff9e158918d5f01207df972220607af
JoseManuelSoriano <jmsb18@gcloud.ua.es>
2024-02-05 19:27:11 +0100

    controlar cambio de velocidad en decision_hunted y subida de velociadad a la araña

--- src/utils/bt/decision_playerhunted.hpp
+++ src/utils/bt/decision_playerhunted.hpp
@@ -7,10 +7,16 @@ struct BTDecisionPlayerHunted : BTNode_t{
     BTNodeStatus_t run(EntityContext_t& ectx) noexcept final { // final es como override sin dejar sobreescribir
         ectx.ai.bh = "Player hunted?";
         if(ectx.em.getSingleton<BlackBoard_t>().playerhunted){
-            ectx.phy.max_speed = 0.5;
+            if(!ectx.ai.boofedvelocity){
+                ectx.phy.max_speed += 0.2;
+            }
+            ectx.ai.boofedvelocity = true;
             return BTNodeStatus_t::success;
         }else{
-            ectx.phy.max_speed = 0.3;
+            if(ectx.ai.boofedvelocity){
+                ectx.phy.max_speed -= 0.2;
+            }
+            ectx.ai.boofedvelocity = false;
             return BTNodeStatus_t::fail;
         }
     }
