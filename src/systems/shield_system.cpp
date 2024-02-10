43a9621d46d1734deedd580b52125b6e825ab58c
JoseManuelSoriano <jmsb18@gcloud.ua.es>
2024-02-10 14:50:43 +0100

    decision_ crear escudo protector a subditos

--- src/systems/shield_system.cpp
+++ src/systems/shield_system.cpp
@@ -9,36 +9,41 @@ void ShieldSystem::update(EntityManager& em) {
 
         if (shi.activeShield)
         {
-            // Recuperamos el player
-            auto& li = em.getSingleton<LevelInfo>();
-            auto* playerEn = em.getEntityByID(li.playerID);
+            if(shi.createdby == EntitieswithShield::Player){
+                // Recuperamos el player
+                auto& li = em.getSingleton<LevelInfo>();
+                auto* playerEn = em.getEntityByID(li.playerID);
 
-            if (playerEn)
-            {
-                if (ent.hasComponent<RenderComponent>() && ent.hasComponent<PhysicsComponent>() &&
-                    playerEn->hasComponent<PhysicsComponent>() && playerEn->hasComponent<InputComponent>())
+                if (playerEn)
                 {
-                    auto& phy_pl = em.getComponent<PhysicsComponent>(*playerEn);
-                    auto& p = em.getComponent<PhysicsComponent>(ent);
-                    auto& r = em.getComponent<RenderComponent>(ent);
+                    if (ent.hasComponent<RenderComponent>() && ent.hasComponent<PhysicsComponent>() &&
+                        playerEn->hasComponent<PhysicsComponent>() && playerEn->hasComponent<InputComponent>())
+                    {
+                        auto& phy_pl = em.getComponent<PhysicsComponent>(*playerEn);
+                        auto& p = em.getComponent<PhysicsComponent>(ent);
+                        auto& r = em.getComponent<RenderComponent>(ent);
 
-                    // Reposicionamos el escudo segun el player
-                    p.position = phy_pl.position;
-                    p.orientation = phy_pl.orientation;
+                        // Reposicionamos el escudo segun el player
+                        p.position = phy_pl.position;
+                        p.orientation = phy_pl.orientation;
 
-                    static const double SHIELD_DISTANCE = 0.75;
+                        static const double SHIELD_DISTANCE = 0.75;
 
-                    // Calculamos el desplazamiento basado en la orientación del jugador
-                    double offsetX = sin(p.orientation) * SHIELD_DISTANCE;
-                    double offsetZ = cos(p.orientation) * SHIELD_DISTANCE;
+                        // Calculamos el desplazamiento basado en la orientación del jugador
+                        double offsetX = sin(p.orientation) * SHIELD_DISTANCE;
+                        double offsetZ = cos(p.orientation) * SHIELD_DISTANCE;
 
-                    // Aplicamos el desplazamiento al escudo
-                    p.position += { offsetX, 0.0f, offsetZ };
-                    if (offsetX == 0.75 || offsetX == -0.75)
-                        r.scale = { 0.5f, 1.0f, 1.0f };
-                    else
-                        r.scale = { 1.0f, 1.0f, 0.5f };
+                        // Aplicamos el desplazamiento al escudo
+                        p.position += { offsetX, 0.0f, offsetZ };
+                        if (offsetX == 0.75 || offsetX == -0.75)
+                            r.scale = { 0.5f, 1.0f, 1.0f };
+                        else
+                            r.scale = { 1.0f, 1.0f, 0.5f };
+                    }
                 }
+            }
+            if(shi.createdby == EntitieswithShield::Subdito){
+                
             }
         }
         else
