937ba29300804e661627d68b86e5900c29c0ea79
JoseManuelSoriano <jmsb18@gcloud.ua.es>
2024-02-19 10:31:55 +0100

    clase grafo y conexión

---
+++ src/utils/pf/Conection.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <utils/types.hpp>
+
+struct Conection
+{
+    /* data */
+    //devuelve el coste que tiene esa conexión
+    // getCost(conection)
+    //devuelve el nodo del que sale esa conexion
+    //getfromnode(conection)
+    //devuelve el nodo al cuál llega esa conexión
+    //getToNode(conection)
+};
---
+++ src/utils/pf/Graph.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <utils/types.hpp>
+
+struct Graph
+{
+    /* data */
+    //array de conexiones
+    //funcion getconections(uint16_t node);
+};
