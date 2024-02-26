1ff568e9bc036b4968cad5e17cb346ed09536bae
JoseManuelSoriano <jmsb18@gcloud.ua.es>
2024-02-26 18:25:13 +0100

    raycast implementado

--- src/utils/bt/decision_playerdetected.hpp
+++ src/utils/bt/decision_playerdetected.hpp
@@ -8,7 +8,7 @@ struct BTDecisionPlayerDetected : BTNode_t{
     BTDecisionPlayerDetected()  {}
 
     BTNodeStatus_t run(EntityContext_t& ectx) noexcept final { // final es como override sin dejar sobreescribir
-         ectx.ai.bh = "detecting player";
+        ectx.ai.bh = "detecting player";
         auto& li = ectx.em.getSingleton<LevelInfo>();
         auto* playerEn = ectx.em.getEntityByID(li.playerID);
         if (not playerEn) return BTNodeStatus_t::fail; // No hay player
@@ -28,24 +28,39 @@ struct BTDecisionPlayerDetected : BTNode_t{
         double angle = acos(dotProduct);
         // Convierte el ángulo de radianes a grados
         double angleDegrees = angle * 180.0f / PI;
-        if(ectx.ent.hasTag<SpiderTag>()){
+        if(ectx.ent.hasTag<SnowmanTag>()){
             std::cout << "AngleDeggreess:" << angleDegrees << "\n";
         }
         // Comprueba si el ángulo está dentro del rango de visión del enemigo
         if (angleDegrees < ectx.ai.field_of_view / 2.0) {
-            //Calculo la distancia del player al enemigo
-            // auto const distance = (ectx.phy.position - plphy.position).lengthSQ();
-            // //Compruebo si esta dentro del radio de detección
-            // if( distance < (ectx.ai.detect_radius * ectx.ai.detect_radius)){
+            //Raycast
+            bool rayintercepted{false};
+            // Realiza un raycast en la dirección en la que el enemigo está mirando para detectar obstáculos
+            RayCast ray = { ectx.phy.position, enemyDirection };
+            //Compruebo si la caja de colisión de un obstaculo ha colisionado con el rayo
+            for(Entity &ent : ectx.em.getEntities()){
+                if(ent.hasTag<WallTag>()){
+                    auto& col = ectx.em.getComponent<ColliderComponent>(ent);
+                    if(col.boundingBox.intersectsRay(ray.origin,ray.direction)){
+                        // si es el caso el enemigo no puede detectar al player
+                        rayintercepted = true;
+                    }
+                }
+            }
+            if(rayintercepted){
+                ectx.ai.playerdetected = false;
+                return BTNodeStatus_t::fail;
+            }else{
                 ectx.ai.playerdetected = true;
-                // return BTNodeStatus_t::success;
-                // std::cout << "hola \n";
-            // }else{
-            // ectx.ai.playerdetected = false;
-            // }
-        }else{
-            ectx.ai.playerdetected = false;
+                return BTNodeStatus_t::success;
+            }
         }
+        // }else{
+        //     // si el campo de visión no detecta al player
+        //    ectx.ai.playerdetected = false;
+        //    return BTNodeStatus_t::fail;
+        // }
+        return BTNodeStatus_t::fail;
         //Calculamos percepción por oido
         //Calculo la distancia del player al enemigo
         // auto const distance = (ectx.phy.position - plphy.position).lengthSQ();
@@ -59,7 +74,7 @@ struct BTDecisionPlayerDetected : BTNode_t{
         // }else{
         //     ectx.ai.playerdetected = false;
         // }
-        return BTNodeStatus_t::fail;
+        
     }
 
 // private:
