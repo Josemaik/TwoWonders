e7072cbe66df5af792be1c913305ad68d671e59b
Julio Sala Gallardo <juliosalag@gmail.com>
2024-03-02 03:31:30 +0100

    Input mando (falta pressed/released)

--- motor/src/managers/input_manager.cpp
+++ motor/src/managers/input_manager.cpp
@@ -1,11 +1,13 @@
 #include "input_manager.hpp"
 
+// Input-related functions: keyboard
+
 bool InputManager::isKeyPressed(int key){
-    return keyStates[key];
+    return m_keyStates[key];
 }
 
 bool InputManager::isKeyReleased(int key){
-    return keyReleaseStates[key];
+    return m_keyReleaseStates[key];
 }
 
 bool InputManager::isKeyDown(GLFWwindow* window, int key){
@@ -20,9 +22,50 @@ void InputManager::keyCallback(GLFWwindow* window, int key, int, int action, int
     auto* inputManager = static_cast<InputManager*>(glfwGetWindowUserPointer(window));
 
     if(action == GLFW_PRESS)
-        inputManager->keyStates[key] = true;
+        inputManager->m_keyStates[key] = true;
     else{
-        inputManager->keyStates[key] = false;
-        inputManager->keyReleaseStates[key] = true;
+        inputManager->m_keyStates[key] = false;
+        inputManager->m_keyReleaseStates[key] = true;
     }
+}
+
+// Input-related functions: gamepad
+
+bool InputManager::isGamePadAvailable(int gamepad){
+    return glfwJoystickPresent(gamepad);
+}
+
+const char* InputManager::getGamePadName(int gamepad){
+    const char* name = glfwGetJoystickName(gamepad);
+    return name ? name : "Uknown";
+}
+
+bool InputManager::isGamePadButtonPressed(int, int){
+    return true;
+}
+
+bool InputManager::isGamePadButtonReleased(int, int){
+    return true;
+}
+
+bool InputManager::isGamePadButtonDown(int gamepad, int button){
+    int count;
+    auto* buttons =  glfwGetJoystickButtons(gamepad, &count);
+    if(buttons && button < count)
+        return buttons[button] == GLFW_PRESS;
+
+    return false;
+}
+
+bool InputManager::isGamePadButtonUp(int gamepad, int button){
+    return !isGamePadButtonDown(gamepad, button);
+}
+
+void InputManager::joystickCallback(int jid, int event) {
+    InputManager& instance = InputManager::getInstance();
+    instance.gamepadButtonCallback(jid, event);
+}
+
+void InputManager::gamepadButtonCallback(int, int){
+    
 }
--- motor/src/managers/input_manager.hpp
+++ motor/src/managers/input_manager.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <GLFW/glfw3.h>
 #include <unordered_map>
 
@@ -16,11 +17,33 @@ public:
     bool isKeyUp(GLFWwindow* window, int key);
 
     static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-    void cleanKeyReleaseStates(){ keyReleaseStates.clear(); };
+    void cleanm_keyReleaseStates(){ m_keyReleaseStates.clear(); };
 
-    // Gamepad / Mouse
+    // Input-related functions: gamepad
+    bool isGamePadAvailable(int gamepad);
+    const char* getGamePadName(int gamepad);
+    bool isGamePadButtonPressed(int gamepad, int button);  // TODO
+    bool isGamePadButtonReleased(int gamepad, int button); // TODO
+    bool isGamePadButtonDown(int gamepad, int button);
+    bool isGamePadButtonUp(int gamepad, int button);
+
+    static void joystickCallback(int jid, int event);
+    void gamepadButtonCallback(int jid, int event);
+
+    // Mouse
 
 private:
-    std::unordered_map<int, bool> keyStates;
-    std::unordered_map<int, bool> keyReleaseStates;
+    // Key States
+    std::unordered_map<int, bool> m_keyStates;
+    std::unordered_map<int, bool> m_keyReleaseStates;
+
+    // Button States
+    std::unordered_map<int, std::unordered_map<int, bool>> m_gamepadButtonStates;
+    std::unordered_map<int, std::unordered_map<int, bool>> m_gamepadButtonReleaseStates;
+
+    // Instance
+    static InputManager& getInstance(){
+        static InputManager instance;
+        return instance;
+    }
 };
