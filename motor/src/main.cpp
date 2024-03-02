0f9ffa762fff91631d5a776a9ffce830845b196e
Julio Sala Gallardo <juliosalag@gmail.com>
2024-03-02 03:31:30 +0100

    Input mando (falta pressed/released)

--- motor/src/main.cpp
+++ motor/src/main.cpp
@@ -32,6 +32,11 @@ int main(){
         //----- Configure callback -----//
         glfwSetWindowUserPointer(wm.getWindow(), &im);
         glfwSetKeyCallback(wm.getWindow(), InputManager::keyCallback);
+        glfwSetJoystickCallback(InputManager::joystickCallback);
+
+        //----- Gamepad -----//
+        if(im.isGamePadAvailable(0) == 1)
+            std::cout << "Gamepad: " << im.getGamePadName(1) << std::endl;
 
         //----- Load model -----// 
         std::cout << "┌──────┐" << std::endl;
@@ -40,9 +45,11 @@ int main(){
         auto filePath = "assets/dummy.obj";
         auto eModel = loadModel(filePath, nScene, rm);
 
+        //------ Textures -----//
         filePath = "assets/container.jpg";
         auto rTexture = loadTexture(filePath, rm); 
 
+        //------ Shaders -----//
         auto rShaderColor = rm.loadResource<Shader>("src/shaders/color.vs", "src/shaders/color.fs", ShaderType::COLOR);
         auto rShaderTexture = rm.loadResource<Shader>("src/shaders/texture.vs", "src/shaders/texture.fs", ShaderType::TEXTURE);
 
@@ -52,13 +59,12 @@ int main(){
         std::cout << "└──────┘" << std::endl;
         nScene->drawTree();
 
-        //------ Shaders -----//
-        // renm.compilingShaders();
-
         // Main loop
         while(!wm.windowShouldClose()){
 
             // std::cout << "A: " << im.isKeyPressed(KEY_A) << std::endl;
+            // std::cout << "A: " << im.isGamePadButtonDown(0, GLFW_GAMEPAD_BUTTON_A) << std::endl;
+            // std::cout << "A: " << im.isGamePadButtonUp(0, GLFW_GAMEPAD_BUTTON_A) << std::endl;
 
             wm.beginDrawing();
 
@@ -76,9 +82,8 @@ int main(){
 
             // Draw (texture)
             renm.useShader(rShaderTexture);
-
-            // renm.drawTexture(rTexture, {0.0f, 500.0f}, {1.0f, 1.0f, 1.0f, 1.0f});
             renm.drawTextureExtra(rTexture, {10.0f, 60.0f}, 0.0f, 0.6f, {1.0f, 1.0f, 1.0f, 1.0f});
+            // renm.drawTexture(rTexture, {0.0f, 500.0f}, {1.0f, 1.0f, 1.0f, 1.0f});
 
             // Draw (model)
             //eModel->draw(glm::mat4());
@@ -86,7 +91,7 @@ int main(){
             wm.endDrawing();
 
             // Clean Release States
-            im.cleanKeyReleaseStates();
+            im.cleanm_keyReleaseStates();
         }
 
         //----- Unload -----//
